e67deee7a20fff06fa889e8f75dda437f53f7806
Xavier Zachary <xiaxuanzhou@qq.com>
2016-03-31 16:24:49 +0800

    闭包

---
+++ Swift_7_闭包_Demo/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Swift_7_闭包_Demo
+//
+//  Created by Jiayu_Zachary on 16/1/4.
+//  Copyright © 2016年 Zachary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+    }
+    
+    //closure expressions 闭包表达式
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
